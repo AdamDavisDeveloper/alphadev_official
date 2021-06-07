a7f4e400e49ad7e838c641486f81a4d62da82f3e
Adam Davis <adavisdeveloper@gmail.com>
2021-06-07 13:19:46 -0400

    start data json file for timeline

---
+++ profile/js/data.js
@@ -0,0 +1,20 @@
+const timeline = [
+  {
+    title: "Hired at Sionic Mobile",
+    subtitle: "Web Developer",
+    img: "./img/#",
+    imgCaption: "",
+    date: "June 2021",
+    id: 02,
+  },
+  {
+    title: "Started work as freelance developer",
+    subtitle: "Front end web development",
+    img: "./img/#",
+    imgCaption: "",
+    date: "January 2020",
+    id: 01,
+  },
+];
+
+export default timeline;
