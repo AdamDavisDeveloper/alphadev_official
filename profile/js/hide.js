eb1e88dfaa1ef422709fcacf4ba4e9c90daee529
Adam Davis <adavisdeveloper@gmail.com>
2021-06-08 16:34:00 -0400

    create collapsable "read more"

---
+++ profile/js/hide.js
@@ -0,0 +1,10 @@
+const readMore = document.getElementById("ReadMore");
+const moreText = document.getElementById("MoreText");
+
+function show(element) {
+  element.classList.remove("hide");
+}
+
+function hide(element) {
+  element.classList.add("hide");
+}
